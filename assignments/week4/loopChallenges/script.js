c462bb2c7326c578207ae29781084cb40543c288
Lori Trindade <39773598+alora1976@users.noreply.github.com>
2022-06-28 10:25:13 -0700

    2nd attempt

--- assignments/week4/loopChallenges/script.js
+++ assignments/week4/loopChallenges/script.js
@@ -6,7 +6,9 @@ for(var i = 1; i <= 20; i += 2) {
 
 // Decreasing Multiples of 3
 // Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
-for(var i = 100; i >= 0; i -= 3) {
+
+for(var i = 100; i >= -1; i --) {
+    if(i % 3 === 0) 
     console.log(i);
 }
 
